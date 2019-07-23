00e22a9ea35d9d785da6820a835cdb3aef65ac7b
Stanley Sham <stanley.sham94@gmail.com>
2019-07-23 13:38:05 -0400

    completed assignment

---
+++ collections.py
@@ -0,0 +1,21 @@
+collection = { 'data': { 'rooms': 
+    [ { 'id': 1, 'room_number': "201", 'capacity': 50}, { 'id': 2, 'room_number': "301", 'capacity': 200 }, { 'id': 3, 'room_number': "1B", 'capacity': 100}
+    ],
+    'events': [
+      { 'id': 1, 'room_id': 2, 'start_time': 18, 'end_time': 20, 'attendees': 75 },
+      { 'id': 2, 'room_id': 1, 'start_time': 10, 'end_time': 18, 'attendees': 25 },
+      { 'id': 3, 'room_id': 2, 'start_time': 10, 'end_time': 18, 'attendees': 20 },
+      { 'id': 4, 'room_id': 3, 'start_time': 18, 'end_time': 21, 'attendees': 56 },
+    ]
+  }
+}
+
+# Exercise 1
+capacity201 = collection['data']['rooms'][0]['capacity']
+print(capacity201)
+
+# Exercise 2
+for event in collection['data']['events']:
+    if event['room_id'] == 1:
+        print(event)
+        print('ok')
